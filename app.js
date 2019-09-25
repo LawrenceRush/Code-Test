e5b9c2f1f757974ab19eba8841a928b42ad863c8
Lawrence <l.rush7@gmail.com>
2019-09-25 16:57:14 -0700

    made html skeleton

---
+++ app.js
@@ -0,0 +1,35 @@
+function hideChallenge() {
+    var challengePage = document.getElementById("challenge-page");
+    if (challengePage.style.display === "none") {
+        challengePage.style.display = "block";
+    } else {
+        challengePage.style.display = "none";
+    }
+}
+
+function hideQuestion() {
+    var challengePage = document.getElementById("challenge-page");
+    if (challengePage.style.display === "none") {
+        challengePage.style.display = "block";
+    } else {
+        challengePage.style.display = "none";
+    }
+}
+
+function hideQuestion() {
+    var challengePage = document.getElementById("challenge-page");
+    if (challengePage.style.display === "none") {
+        challengePage.style.display = "block";
+    } else {
+        challengePage.style.display = "none";
+    }
+}
+
+function hideQuestion() {
+    var challengePage = document.getElementById("challenge-page");
+    if (challengePage.style.display === "none") {
+        challengePage.style.display = "block";
+    } else {
+        challengePage.style.display = "none";
+    }
+}
